037e74adc3f4acad809ed4944d07021788551cc3
Tanvi Pooranmal Meena <96572616+TanviPooranmal@users.noreply.github.com>
2024-10-12 15:39:36 +0530

    Netlify deployment

--- src/app/doctor/page.tsx
+++ src/app/doctor/page.tsx
@@ -22,7 +22,7 @@ import {
   Search,
 } from "lucide-react";
 
-export function DoctorPage() {
+export default function DoctorPage() {
   return (
     <div className="flex flex-row w-full">
       <SideDrawer />
@@ -103,26 +103,5 @@ function NavItem({
         </span>
       )}
     </Button>
-  );
-}
-import { ProfileForm } from "@/app/doctor/form";
-import { SiteFooter } from "@/components/ui/sitefooter";
-import { SiteHeader } from "@/components/ui/siteheader";
-
-export default function Home() {
-  return (
-    <>
-      <SiteHeader />
-
-      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
-        <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-500/45 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-          <div className="flex space-x-4">
-            <ProfileForm />
-          </div>
-        </div>
-      </div>
-
-      <SiteFooter />
-    </>
   );
 }
